13fa14b6af5bf5e5d83e5782ced2ae49cff72bcd
TheWiseSailor <rudasilljay@gmail.com>
2023-10-17 14:41:42 -0400

    packages made

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

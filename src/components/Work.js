c6d9ff36237152b773f97a2fdc4beedb68347232
TheWiseSailor <rudasilljay@gmail.com>
2023-10-21 17:21:22 -0400

    Notes

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,5 +3,5 @@ import "../styles/App.css";
 function Work() {
   return <div>This is in working progress</div>;
 }
-
+//get the routes working even though the routes are working in local host,make it work for deployed app
 export default Work;

b6e17366609c33df37a112153a38582b22eabeb5
TheWiseSailor <rudasilljay@gmail.com>
2023-10-23 21:58:29 -0400

    scrapping the hamburger menu completely since im only showing 2 pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,8 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; // Add this import
+import { Link } from "react-router-dom";
 import "../styles/App.css";
 
 function Header() {
-  const link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.href = "https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css";
-  document.head.appendChild(link);
-
-  const script = document.createElement("script");
-  script.src = "https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js";
-  document.body.appendChild(script);
-
-  // Initialize AOS after the script has loaded
-  script.onload = () => {
-    window.AOS.init();
-  };
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -32,7 +19,7 @@ function Header() {
         <div className="container mx-auto flex items-center justify-between">
           <Link
             to="/"
-            className="text-blue-300 text-3xl font-bold ml-4 md:ml-0 pl-6"
+            className="text-blue-300 text-xl font-bold ml-4 md:ml-0 pl-6"
             data-aos="fade-down"
             data-aos-duration="3000"
           >
@@ -44,31 +31,27 @@ function Header() {
             <div className={`hamburger-menu ${menuOpen ? "open" : ""}`}></div>
           </div>
 
-          <nav className="hidden md:flex space-x-4">
+          <nav
+            className={`md:flex space-x-4 ${
+              menuOpen ? "mobile-menu-open" : ""
+            }`}
+          >
             <Link
               to="/work"
-              className="text-blue-300 hover:text-gray-400 text-3xl pr-6"
+              className="text-blue-300 hover:text-gray-400 text-xl pr-6"
+              onClick={closeMenu}
             >
               My Work
             </Link>
             <Link
               to="/contact"
-              className="text-blue-300 hover:text-gray-400 text-3xl pr-6"
+              className="text-blue-300 hover:text-gray-400 text-xl pr-6"
+              onClick={closeMenu}
             >
               Contact
             </Link>
           </nav>
         </div>
-      </div>
-
-      {/* Mobile menu */}
-      <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
-        <Link to="/work" onClick={closeMenu}>
-          My Work
-        </Link>
-        <Link to="/contact" onClick={closeMenu}>
-          Contact
-        </Link>
       </div>
     </div>
   );

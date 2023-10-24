35039a62740897b4b1c3147a45c319564f41acf3
TheWiseSailor <rudasilljay@gmail.com>
2023-10-23 20:42:25 -0400

    added a my works section using instagram embed

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "../styles/App.css";
 
+const instagramPostUrls = [
+  "https://www.instagram.com/p/CywwWIWAtvS/",
+  " https://www.instagram.com/p/B64f4aEAytF/",
+  "https://www.instagram.com/p/B3GS0cxgXg2/",
+  "https://www.instagram.com/p/Bps579HgB9m/",
+  "https://www.instagram.com/p/Bp23ZU1lIb9/",
+  "https://www.instagram.com/p/BqI50IAAJnH/",
+];
+
 function Work() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="w-full max-w-md p-4 border border-blue-300 rounded-2xl">
-        <h2 className="text-3xl font-semibold mb-4">My Work</h2>
-        <p>Test</p>
+      <div className="grid grid-cols-3 gap-32">
+        {instagramPostUrls.map((url, index) => (
+          <div key={index} className="flex justify-center ">
+            <iframe
+              src={`${url}embed/`}
+              title={`Instagram Post ${index}`}
+              width="320"
+              height="320" // Set a fixed height here
+              frameBorder="0"
+              scrolling="no"
+              className="rounded-2xl"
+            ></iframe>
+          </div>
+        ))}
       </div>
     </div>
   );
